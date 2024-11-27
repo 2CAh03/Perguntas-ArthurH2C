13b92ef142fa884ffda6a7481bddbc202539aa78
2CAh03 <00001103395051sp@al.educacao.sp.gov.br>
2024-11-27 08:10:26 -0300

    Add files via upload

---
+++ perguntas/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Educação Fisica',
+    'Qual é o maior time do Brasil?',
+    'São Paulo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'história',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz carro em Inglês?',
+    'Carro em Inglês se diz car'
+)
